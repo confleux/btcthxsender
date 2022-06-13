c8289622c2b05ffd84811294c1968196786b23d8
confleux <confleux@gmail.com>
2022-06-13 20:07:37 +0300

    done: refactor, getdata, cli arguments

--- app.ts
+++ app.ts
@@ -1,20 +1,37 @@
 import config from "config";
 import net from "net";
+import { program } from "commander";
 
+import Message from "./message";
 import makeVersionPayload from "./makeVersionPayload";
-import makeMessage from "./message";
+import makeGetdataPayload from "./makeGetdataPayload";
+
+program
+  .option("-v, --version")
+  .option("-g, --getdata");
+program.parse(process.argv);
+const options = program.opts();
 
 const port: number = config.get("port");
 const ip: string = config.get("ip");
-
-const version  = makeVersionPayload();
-const message = makeMessage("version", version);
 
 const client = new net.Socket();
 
 client.connect(port, ip, () => {
   console.log(`Connected to: ${ip}:${port}`);
-  client.write(message);
+  if (options.version) {
+    const versionPayload  = makeVersionPayload();
+    const versionMessage = new Message("version", versionPayload).getData();
+
+    client.write(versionMessage);
+  }
+
+  if (options.getdata) {
+    const getdataPayload = makeGetdataPayload("36b8993e04ccbb5229ecc7752a4dbe2e32dc880b128db80a3709ed40c0ab83f6", 1);
+    const getdataMessage = new Message("getdata", getdataPayload).getData();
+
+    client.write(getdataMessage);
+  }
 });
 
 client.on('data', (data) => {
--- message.ts
+++ message.ts
@@ -2,19 +2,29 @@ import config from "config";
 import pack from "./utils/pack";
 import { createHash } from "crypto";
 
-const makeMessage = (cmd: string, payload: Buffer): Buffer => {
-  const magic = pack("<L", config.get("magic"));
+export default class Message {
+   #data: Buffer;
+   #payload: Buffer;
+   #command: string;
 
-  const cmds: string = (cmd + "\0".repeat(12 - cmd.length));
-  const command = pack("<12s", cmds);
+   constructor(cmd: string, payload: Buffer) {
+     this.#payload = payload;
+     this.#command = cmd;
 
-  const length = pack("<I", payload.length);
+     const magic = pack("<L", config.get("magic"));
 
-  const hash1 = createHash('sha256').update(payload).digest();
-  const hash2 = createHash('sha256').update(hash1).digest();
-  const check = hash2.subarray(0, 4);
+     const command = pack("<12s", (cmd + "\0".repeat(12 - cmd.length)));
 
-  return Buffer.concat([magic, command, length, check, payload]);
+     const length = pack("<I", payload.length);
+
+     const hash1 = createHash('sha256').update(payload).digest();
+     const hash2 = createHash('sha256').update(hash1).digest();
+     const checksum = hash2.subarray(0, 4);
+
+     this.#data = Buffer.concat([magic, command, length, checksum, payload]);
+   }
+
+    getData(): Buffer {
+     return this.#data;
+   }
 }
-
-export default makeMessage;
