853350ce10f62944806e30f05948b787b1f335a7
confleux <confleux@gmail.com>
2022-06-14 20:33:34 +0300

    done: refactor, getdata, cli arguments

---
+++ makeGetdataPayload.ts
@@ -0,0 +1,11 @@
+import pack from "./utils/pack";
+
+const makeGetdataPayload = (id: string, dataType: number) => {
+  const count = pack("<b", 1);
+  const type = pack("<i", dataType);
+  const hash = Buffer.from(id, "hex");
+
+  return Buffer.concat([count, type, hash]);
+}
+
+export default makeGetdataPayload;
