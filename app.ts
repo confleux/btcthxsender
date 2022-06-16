e502919fb0cb6800a74e80ec51d4c7572dc2fc60
confleux <confleux@gmail.com>
2022-06-17 01:06:02 +0300

    done: tx

--- app.ts
+++ app.ts
@@ -5,10 +5,12 @@ import { program } from "commander";
 import Message from "./message";
 import makeVersionPayload from "./makeVersionPayload";
 import makeGetdataPayload from "./makeGetdataPayload";
+import makeTxPayload from "./makeTxPayload";
 
 program
   .option("-v, --version")
-  .option("-g, --getdata");
+  .option("-g, --getdata")
+  .option("-t, --transaction");
 program.parse(process.argv);
 const options = program.opts();
 
@@ -17,27 +19,45 @@ const ip: string = config.get("ip");
 
 const client = new net.Socket();
 
-client.connect(port, ip, () => {
-  console.log(`Connected to: ${ip}:${port}`);
-  if (options.version) {
-    const versionPayload  = makeVersionPayload();
-    const versionMessage = new Message("version", versionPayload).getData();
+const previous_output: string = config.get("tx.previous_output");
+const receiver_address: string = config.get("tx.receiver_address");
+const my_address: string = config.get("tx.my_address");
+const private_key: string = config.get("tx.private_key");
 
-    client.write(versionMessage);
-  }
+ client.connect(port, ip, () => {
+   console.log(`Connected to: ${ip}:${port}`);
+   if (options.version) {
+     console.log("Sending version message");
 
-  if (options.getdata) {
-    const getdataPayload = makeGetdataPayload("36b8993e04ccbb5229ecc7752a4dbe2e32dc880b128db80a3709ed40c0ab83f6", 1);
-    const getdataMessage = new Message("getdata", getdataPayload).getData();
+     const versionPayload  = makeVersionPayload();
+     const versionMessage = new Message("version", versionPayload);
 
-    client.write(getdataMessage);
-  }
-});
+     client.write(versionMessage.getData());
+   }
 
-client.on('data', (data) => {
-  console.log(data.toString("hex"));
-});
+   if (options.getdata) {
+     console.log("Sending getdata message");
 
-client.on('close', () => {
-  console.log("Connection closed");
-});
+     const getdataPayload = makeGetdataPayload("36b8993e04ccbb5229ecc7752a4dbe2e32dc880b128db80a3709ed40c0ab83f6", 1);
+     const getdataMessage = new Message("getdata", getdataPayload);
+
+     client.write(getdataMessage.getData());
+   }
+
+   if (options.transaction) {
+     console.log("Sending tx message");
+
+     const txPayload = makeTxPayload(previous_output, receiver_address, my_address, private_key);
+     const txMessage = new Message("tx", txPayload);
+
+     client.write(txMessage.getData());
+   }
+ });
+
+ client.on('data', (data) => {
+   console.log(data.toString("hex"));
+ });
+
+ client.on('close', () => {
+   console.log("Connection closed");
+ });
--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,11 @@
 export default {
   magic: 3652501241,
   port: 8333,
-  ip: "178.62.87.120"
+  ip: "23.175.0.212",
+  tx: {
+    previous_output: "badb5277bdc692b1f073ef61d74116665cc9a6a2d40d753290b7ccc2c5f81432",
+    receiver_address: "2MuWaNQCuWvazwyF9GyBKBrCj9GpBT8bfFK",
+    my_address: "mkURxp8LKfapYHnhUKM6cSSmcPT5dXerXN",
+    private_key: "cSEZs7jVqkmWs5tUf5QdoXtJ4GCze4atTptChdUYJ41qkUCTAjBX"
+  }
 }
